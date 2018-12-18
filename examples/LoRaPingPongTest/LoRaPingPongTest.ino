eec48369a9c888623c78a0d7b37d2232ab1e1493
Rooney Jang <rooney.jang@codezoo.co.kr>
2018-12-18 23:50:04 +0900

    setup function add
    Signed-off-by: Rooney Jang <rooney.jang@codezoo.co.kr>

--- examples/LoRaPingPongTest/LoRaPingPongTest.ino
+++ examples/LoRaPingPongTest/LoRaPingPongTest.ino
@@ -10,7 +10,8 @@
 #define RXpin 10
 #define ATSerial Serial
 
-String lora_app_key = "11 22 33 44 55 66 77 88 99 aa bb cc dd ee ff 00";  //16byte hex key
+//16byte hex key
+String lora_app_key = "11 22 33 44 55 66 77 88 99 aa bb cc dd ee ff 00";  
 
 SoftwareSerial DebugSerial(RXpin,TXpin);
 SNIPE SNIPE(ATSerial);
@@ -23,15 +24,35 @@ void setup() {
   while(!ATSerial);
 
   DebugSerial.begin(115200);
+
   /* SNIPE LoRa Initialization */
   if (!SNIPE.lora_init()) {
     DebugSerial.println("SNIPE LoRa Initialization Fail!");
     while (1);
   }
-  /* SNIPE LoRa Set AppKey */
+
+  /* SNIPE LoRa Set Appkey */
   if (!SNIPE.lora_setAppKey(lora_app_key)) {
     DebugSerial.println("SNIPE LoRa app key value has not been changed");
   }
+  
+  /* SNIPE LoRa Set Frequency */
+  if (!SNIPE.lora_setFreq(LORA_CH_1)) {
+    DebugSerial.println("SNIPE LoRa Frequency value has not been changed");
+  }
+
+  /* SNIPE LoRa Set Spreading Factor */
+  if (!SNIPE.lora_setSf(LORA_SF_7)) {
+    DebugSerial.println("SNIPE LoRa Sf value has not been changed");
+  }
+
+  /* SNIPE LoRa Set Rx Timeout 
+   * If you select LORA_SF_12, 
+   * RX Timout use a value greater than 5000  
+  */
+  if (!SNIPE.lora_setRxtout(5000)) {
+    DebugSerial.println("SNIPE LoRa Rx Timout value has not been changed");
+  }  
     
   DebugSerial.println("SNIPE LoRa PingPong Test");
 }
